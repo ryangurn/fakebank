49e0cc681fc10e58fa4c0a4dc897ee5ab97f47e9
ryangurn <ryangurnick@gmail.com>
2019-12-12 21:45:48 -0800

    Adding Resources

---
+++ resources/lang/en/general.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /**
+     * General Translations to start with
+     */
+
+    'Name' => 'Name',
+    'Caption' => 'Caption',
+    'Active' => 'Active',
+    'Not Configured' => 'Not Configured',
+    'None Configured' => 'None Configured'
+];
