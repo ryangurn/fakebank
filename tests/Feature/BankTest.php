25d5dfcb414fd9f559b4ad0be633a779aa8bef18
ryangurn <ryangurnick@gmail.com>
2020-03-14 09:42:15 -0700

    Adding Tests
    Most of these files are just the stubs being committed... Additional test cases and assertions will follow on this branch

--- tests/Feature/BankTest.php
+++ tests/Feature/BankTest.php
@@ -10,7 +10,7 @@ class BankTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testBankIndex(){
+    public function testBankIndexPage(){
 
         $user = factory(User::class)->create();
 
@@ -18,5 +18,29 @@ class BankTest extends TestCase
         $response = $this->actingAs($user)->get(route('bank.index'));
         $response->assertStatus(200);
         $response->assertSee('Bank');
+    }
+
+    public function testBankCreatePage(){
+
+        $user = factory(User::class)->create();
+
+        $this->followingRedirects();
+        $response = $this->actingAs($user)->get(route('bank.create'));
+        $response->assertStatus(200);
+        $response->assertSee('Create Bank');
+        $response->assertSee('Bank Name');
+        $response->assertSee('Bank Caption');
+        $response->assertSee('Trolls to enable, this must be in JSON form.');
+    }
+
+    public function testBankCreateForm(){
+
+        $user = factory(User::class)->create();
+
+        $this->followingRedirects();
+        $data = ['bank' => 'Fakebank', 'caption' => 'The fakest of them all', 'trolls' => ''];
+        $response = $this->actingAs($user)->post(route('bank.store'), $data);
+        $response->assertStatus(200);
+
     }
 }
---
+++ tests/Feature/TransactionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TransactionTest extends TestCase
+{
+
+}
