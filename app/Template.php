373f4134b054d36036fe461be8f5de5cb5c61e66
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-06 22:08:16 -0700

    Adding Template Model

---
+++ app/Template.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Template extends Model
+{
+    /**
+     * be explicit, yes means yes.
+     * @var string
+     */
+    protected $table = 'templates';
+
+    /**
+     * columns that are fillable via the ::create()
+     * method using normal eloquent setters
+     * @var string[]
+     */
+    protected $fillable = [
+        'bank_id',
+        'settings',
+        'resource'
+    ];
+
+    /**
+     * casting of settings back to an array
+     * from json store in the mariadb column.
+     * @var string[]
+     */
+    protected $casts = [
+        'settings' => 'array'
+    ];
+
+    /**
+     * @return HasOne
+     */
+    public function bank() {
+        return $this->hasOne(Bank::class, 'id', 'bank_id');
+    }
+}
