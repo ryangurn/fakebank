38eb876e511d118333cdfd32f11b3366117541ca
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-10 17:57:46 -0700

    Setting up variables table.

---
+++ database/migrations/2020_09_11_002742_create_templates_variables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemplatesVariablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates_variables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('template_id');
+            $table->string('variable');
+            $table->longText('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates_variables');
+    }
+}
