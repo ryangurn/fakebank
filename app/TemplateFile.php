67d0183710743e72ec26e2ba6a497eec30a706e1
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-21 22:56:11 -0700

    Adding migration & model

---
+++ app/TemplateFile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class TemplateFile extends Model
+{
+    use LogsActivity;
+
+    protected static $logFillable = true;
+    protected static $logName = 'file';
+
+    protected $table = 'templates_files';
+
+    protected $fillable = [
+        'template_id',
+        'storage',
+        'type'
+    ];
+
+
+}
---
+++ database/migrations/2020_09_22_005522_create_templates_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemplateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('templates_files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('template_id');
+            $table->text('storage');
+            $table->integer('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('templates_files');
+    }
+}
