7096ce9a4d9d9e9c56d1508d54326d610d602e06
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-26 16:32:58 -0700

    Adding Route Model
    This will create the model for routes so that we can set these via the information in the db.

---
+++ app/TemplateRoute.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+/**
+ * Class TemplateRoute
+ * @package App
+ */
+class TemplateRoute extends Model
+{
+    use LogsActivity;
+
+    /**
+     * @var bool
+     */
+    protected static $logFillable = true;
+    /**
+     * @var string
+     */
+    protected static $logName = 'route';
+
+    /**
+     * @var string
+     */
+    protected $table = 'templates_routes';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'template_id',
+        'file_id',
+        'route',
+    ];
+
+    /**
+     * @return HasOne
+     */
+    public function template() {
+        return $this->hasOne(Template::class, 'id', 'template_id');
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function file() {
+        return $this->hasOne(TemplateFile::class, 'id', 'file_id');
+    }
+}
