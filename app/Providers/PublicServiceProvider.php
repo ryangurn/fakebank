69efa7fe97994c1d43380ee2d529d348541838e3
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-28 18:25:33 -0700

    Adding variable support
    Adding support for variables to the public service provider, this will send all variables to all views with specific routes.

--- app/Providers/PublicServiceProvider.php
+++ app/Providers/PublicServiceProvider.php
@@ -24,9 +24,20 @@ class PublicServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('public.index', function($view){
-            $activeBank = Bank::where('settings->status', '=', 'true')->first();
-            $view->with('activeBank', $activeBank);
+        $bank = Bank::where('settings->status', '=', 'true')->first();
+        if ($bank == null) return;
+
+        view()->composer('public.index', function($view) use ($bank){
+            $view->with('activeBank', $bank);
         });
+
+        foreach ($bank->template->routes as $route) {
+            $view = explode(".", $route->file->storage);
+            view()->composer('public.' . $route->template->resource . '.' . strtolower($route->file->type) . 's.' . $view[0], function($view) use ($bank) {
+                foreach ($bank->template->variables as $variable) {
+                    $view->with($variable->variable, $variable->value);
+                }
+            });
+        }
     }
 }
