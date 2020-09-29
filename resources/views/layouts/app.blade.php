b9b37458e8ae32d81c64792941b504f5fdeb6675
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-29 14:28:33 -0700

    Adding Logs to Menus

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,15 @@
                                     <a class="dropdown-item" href="{{ route('transaction.index')  }}">Transactions</a>
                                 </div>
                             </li>
+                            <li class="nav-item dropdown">
+                                <a id="adminDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" v-pre>
+                                    Admin <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="{{ route('log.index') }}">Logs</a>
+                                </div>
+                            </li>
 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
