83ef4244ff9ca507c0fb7f32e6160792fcf0a411
Ryan Gurnick <ryangurnick@gmail.com>
2020-09-30 15:30:04 -0700

    Adding @store to user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\SendTemporaryPassword;
 use App\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
+    public $validator = [
+        'name' => 'required|alphadash',
+        'email' => 'required|email'
+    ];
+
+    public $messages = [
+        'name.required' => 'A name is required',
+        'name.alphadash' => 'Alpha numeric characters only (including dash and underscore)',
+        'email.required' => 'An email is required',
+        'email.email' => 'Please provide a valid email'
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -22,7 +40,7 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response
+     * @return Application|Factory|Response|View
      */
     public function create()
     {
@@ -38,14 +56,35 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = validator($request->all(), $this->validator, $this->messages);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator);
+        }
+
+        # generate password
+        $tmpPassword = Str::random(16);
+
+        # add user record
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($tmpPassword)
+        ]);
+
+
+        # send email with password
+        $user->notify(new SendTemporaryPassword($tmpPassword));
+        $user->sendEmailVerificationNotification();
+
+        return redirect()->route('user.index')->with('success', 'User created and notified of account creation.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return Response
+     * @param User $user
+     * @return Application|Factory|Response|View
      */
     public function show(User $user)
     {
