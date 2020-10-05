6b8e6e860f3e6027c6fbed863fdd60f8731fa04f
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-05 11:30:56 -0700

    Adding logging for the login action

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -37,4 +38,13 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function authenticated(Request $request, $user)
+    {
+        activity('user')
+            ->causedBy($user)
+            ->performedOn($user)
+            ->log('logged in');
+    }
+
 }
