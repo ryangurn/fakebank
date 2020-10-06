0c16142ff635c2852c6ed2f94dcbbb635808ae9d
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-05 21:13:18 -0700

    Updating app.php and adding permissions config.

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Creativeorange\Gravatar\GravatarServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
