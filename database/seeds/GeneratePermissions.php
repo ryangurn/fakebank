bc6bc75e0a2c9ef70e8c35390a1462b91e4e3927
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-06 12:51:37 -0700

    Assigning Admin Permissions

--- database/seeds/GeneratePermissions.php
+++ database/seeds/GeneratePermissions.php
@@ -1,6 +1,7 @@
 <?php
 
-use \Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
 
 class GeneratePermissions extends Seeder
@@ -12,57 +13,125 @@ class GeneratePermissions extends Seeder
      */
     public function run()
     {
-        Permission::firstOrCreate(['name' => 'create user']);
-        Permission::firstOrCreate(['name' => 'read user']);
-        Permission::firstOrCreate(['name' => 'update user']);
-        Permission::firstOrCreate(['name' => 'delete user']);
-        Permission::firstOrCreate(['name' => 'access user']);
-        Permission::firstOrCreate(['name' => 'send temporary password']);
+        // arrays
+        $adminArr = [];
 
-        Permission::firstOrCreate(['name' => 'create bank']);
-        Permission::firstOrCreate(['name' => 'read bank']);
-        Permission::firstOrCreate(['name' => 'update bank']);
-        Permission::firstOrCreate(['name' => 'delete bank']);
-        Permission::firstOrCreate(['name' => 'access bank']);
-        Permission::firstOrCreate(['name' => 'activate bank']);
+        // get roles for assignment
+        $admin = Role::firstOrCreate(['name' => 'admin']);
+        $staff = Role::firstOrCreate(['name' => 'staff']);
+        $editor = Role::firstOrCreate(['name' => 'editor']);
+        $reporter = Role::firstOrCreate(['name' => 'reporter']);
+        $customer = Role::firstOrCreate(['name' => 'customer']);
+        $banker = Role::firstOrCreate(['name' => 'banker']);
 
-        Permission::firstOrCreate(['name' => 'create account']);
-        Permission::firstOrCreate(['name' => 'read account']);
-        Permission::firstOrCreate(['name' => 'update account']);
-        Permission::firstOrCreate(['name' => 'delete account']);
-        Permission::firstOrCreate(['name' => 'access account']);
-        Permission::firstOrCreate(['name' => 'generate transactions']);
+        $user_create = Permission::firstOrCreate(['name' => 'create user']);
+        $user_read = Permission::firstOrCreate(['name' => 'read user']);
+        $user_update = Permission::firstOrCreate(['name' => 'update user']);
+        $user_delete = Permission::firstOrCreate(['name' => 'delete user']);
+        $user_access = Permission::firstOrCreate(['name' => 'access user']);
+        $user_tmp = Permission::firstOrCreate(['name' => 'send temporary password']);
+        // user assignments
+        $adminArr[] = $user_create;
+        $adminArr[] = $user_read;
+        $adminArr[] = $user_update;
+        $adminArr[] = $user_delete;
+        $adminArr[] =$user_access;
+        $adminArr[] = $user_tmp;
 
-        Permission::firstOrCreate(['name' => 'create transaction']);
-        Permission::firstOrCreate(['name' => 'read transaction']);
-        Permission::firstOrCreate(['name' => 'update transaction']);
-        Permission::firstOrCreate(['name' => 'delete transaction']);
-        Permission::firstOrCreate(['name' => 'access transaction']);
+        $bank_create = Permission::firstOrCreate(['name' => 'create bank']);
+        $bank_read = Permission::firstOrCreate(['name' => 'read bank']);
+        $bank_update = Permission::firstOrCreate(['name' => 'update bank']);
+        $bank_delete = Permission::firstOrCreate(['name' => 'delete bank']);
+        $bank_access = Permission::firstOrCreate(['name' => 'access bank']);
+        $bank_activate = Permission::firstOrCreate(['name' => 'activate bank']);
+        // bank assignments
+        $adminArr[] = $bank_create;
+        $adminArr[] = $bank_read;
+        $adminArr[] = $bank_update;
+        $adminArr[] = $bank_delete;
+        $adminArr[] = $bank_access;
+        $adminArr[] = $bank_activate;
 
-        Permission::firstOrCreate(['name' => 'create template']);
-        Permission::firstOrCreate(['name' => 'read template']);
-        Permission::firstOrCreate(['name' => 'update template']);
-        Permission::firstOrCreate(['name' => 'delete template']);
-        Permission::firstOrCreate(['name' => 'access template']);
 
-        Permission::firstOrCreate(['name' => 'create variable']);
-        Permission::firstOrCreate(['name' => 'read variable']);
-        Permission::firstOrCreate(['name' => 'update variable']);
-        Permission::firstOrCreate(['name' => 'delete variable']);
-        Permission::firstOrCreate(['name' => 'access variable']);
+        $account_create = Permission::firstOrCreate(['name' => 'create account']);
+        $account_read = Permission::firstOrCreate(['name' => 'read account']);
+        $account_update = Permission::firstOrCreate(['name' => 'update account']);
+        $account_delete = Permission::firstOrCreate(['name' => 'delete account']);
+        $account_access = Permission::firstOrCreate(['name' => 'access account']);
+        $account_generate = Permission::firstOrCreate(['name' => 'generate transactions']);
+        // account assignments
+        $adminArr[] = $account_create;
+        $adminArr[] = $account_read;
+        $adminArr[] = $account_update;
+        $adminArr[] = $account_delete;
+        $adminArr[] = $account_access;
+        $adminArr[] = $account_generate;
 
-        Permission::firstOrCreate(['name' => 'create route']);
-        Permission::firstOrCreate(['name' => 'read route']);
-        Permission::firstOrCreate(['name' => 'update route']);
-        Permission::firstOrCreate(['name' => 'delete route']);
-        Permission::firstOrCreate(['name' => 'access route']);
+        $transaction_create = Permission::firstOrCreate(['name' => 'create transaction']);
+        $transaction_read = Permission::firstOrCreate(['name' => 'read transaction']);
+        $transaction_update = Permission::firstOrCreate(['name' => 'update transaction']);
+        $transaction_delete = Permission::firstOrCreate(['name' => 'delete transaction']);
+        $transaction_access = Permission::firstOrCreate(['name' => 'access transaction']);
+        // transaction assignments
+        $adminArr[] = $transaction_create;
+        $adminArr[] = $transaction_read;
+        $adminArr[] = $transaction_update;
+        $adminArr[] = $transaction_delete;
+        $adminArr[] = $transaction_access;
 
-        Permission::firstOrCreate(['name' => 'create file']);
-        Permission::firstOrCreate(['name' => 'update file']);
-        Permission::firstOrCreate(['name' => 'read file']);
-        Permission::firstOrCreate(['name' => 'delete file']);
-        Permission::firstOrCreate(['name' => 'access file']);
+        $template_create = Permission::firstOrCreate(['name' => 'create template']);
+        $template_read = Permission::firstOrCreate(['name' => 'read template']);
+        $template_update = Permission::firstOrCreate(['name' => 'update template']);
+        $template_delete = Permission::firstOrCreate(['name' => 'delete template']);
+        $template_access = Permission::firstOrCreate(['name' => 'access template']);
+        // template assignments
+        $adminArr[] = $template_create;
+        $adminArr[] = $template_read;
+        $adminArr[] = $template_update;
+        $adminArr[] = $template_delete;
+        $adminArr[] = $template_access;
 
-        Permission::firstOrCreate(['name' => 'access log']);
+        $variable_create = Permission::firstOrCreate(['name' => 'create variable']);
+        $variable_read = Permission::firstOrCreate(['name' => 'read variable']);
+        $variable_update = Permission::firstOrCreate(['name' => 'update variable']);
+        $variable_delete = Permission::firstOrCreate(['name' => 'delete variable']);
+        $variable_access = Permission::firstOrCreate(['name' => 'access variable']);
+        // variable assignments
+        $adminArr[] = $variable_create;
+        $adminArr[] = $variable_read;
+        $adminArr[] = $variable_update;
+        $adminArr[] = $variable_delete;
+        $adminArr[] = $variable_access;
+
+        $route_create = Permission::firstOrCreate(['name' => 'create route']);
+        $route_read = Permission::firstOrCreate(['name' => 'read route']);
+        $route_update = Permission::firstOrCreate(['name' => 'update route']);
+        $route_delete = Permission::firstOrCreate(['name' => 'delete route']);
+        $route_access = Permission::firstOrCreate(['name' => 'access route']);
+        // route assignments
+        $adminArr[] = $route_create;
+        $adminArr[] = $route_read;
+        $adminArr[] = $route_update;
+        $adminArr[] = $route_delete;
+        $adminArr[] = $route_access;
+
+        $file_create = Permission::firstOrCreate(['name' => 'create file']);
+        $file_update = Permission::firstOrCreate(['name' => 'update file']);
+        $file_read = Permission::firstOrCreate(['name' => 'read file']);
+        $file_delete = Permission::firstOrCreate(['name' => 'delete file']);
+        $file_access = Permission::firstOrCreate(['name' => 'access file']);
+        // file assignments
+        $adminArr[] = $file_create;
+        $adminArr[] = $file_update;
+        $adminArr[] = $file_read;
+        $adminArr[] = $file_delete;
+        $adminArr[] = $file_access;
+
+        $log_access = Permission::firstOrCreate(['name' => 'access log']);
+        // log assignments
+        $adminArr[] = $log_access;
+
+            // sync permissions
+        $admin->syncPermissions($adminArr);
     }
 }
